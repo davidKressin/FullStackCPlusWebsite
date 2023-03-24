40f5cb9826601fd227d9b99fbb0aec6a0aa620cd
david kressin <david@garciagonzales.com>
2023-03-24 13:21:23 -0300

    Path * agregado

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
-
+app.use("*", (req, res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+})
 
 
 // Escuchar peticiones
